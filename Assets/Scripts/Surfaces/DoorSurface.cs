e953734defaa971a8519632c9fe49c9ed6916d68
spicy <spiceycurrey@gmail.com>
2022-09-06 17:59:07 -0700

    Refactored surface override particle system logic

--- Assets/Scripts/Surfaces/DoorSurface.cs
+++ Assets/Scripts/Surfaces/DoorSurface.cs
@@ -11,14 +11,16 @@ namespace Detection
         [SerializeField] ParticleSystem _particleSystem;
         void IScannable.EmitParticle(Vector3 position, ParticleSystem overrideParticleSystem)
         {
-            if (_particleSystem == null) return;
-
             var emitArgs = new ParticleSystem.EmitParams();
             emitArgs.position = position;
             emitArgs.velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
-            if (overrideParticleSystem != null) overrideParticleSystem.Emit(emitArgs, 1);
-            else _particleSystem.Emit(emitArgs, 1);
+            if (overrideParticleSystem == null)
+            {
+                if (_particleSystem == null) return;
+                _particleSystem.Emit(emitArgs, 1);
+            }
+            else overrideParticleSystem.Emit(emitArgs, 1);
         }
     }
 }
