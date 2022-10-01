7e0d7507d281a5ed983292f5470c1442cdf6a445
spicy <spiceycurrey@gmail.com>
2022-10-01 11:34:33 -0700

    Modified HandBoneData

--- Assets/Scripts/HandBoneData.cs
+++ Assets/Scripts/HandBoneData.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class HandBoneData : MonoBehaviour
 {
     public enum HandModelSide { Left, Right };
+    public enum HandModelPose { Primary, Secondary };
 
     public HandModelSide handSide;
+    public HandModelPose poseType;
+
     public Transform root;
     public Animator animator;
     public Transform[] fingerBones;
