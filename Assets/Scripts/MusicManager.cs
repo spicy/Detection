da29d44aa9d3336fdf13341d783261fd8a6cc31a
Daniel Currey <spiceycurrey@gmail.com>
2022-10-22 15:04:43 -0700

    Added music analyzer

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -5,101 +5,114 @@ using UnityEngine;
 using UnityEngine.Audio;
 
 // Usage example; FindObjectOfType<MusicManager>();
-public class MusicManager : MonoBehaviour
+namespace Detection
 {
-	public static MusicManager musicManager;
-	public AudioMixerGroup audioMxrGroup;
-	public List<Sound> musicList;
-	public bool randomize = false;
-
-	private int currentSongIndex = 0;
-	private List<Sound> haventPlayedList;
-
-	void Awake()
+	public class MusicManager : MonoBehaviour
 	{
-		haventPlayedList = new List<Sound>();
+		public static MusicManager musicManager;
+		public AudioMixerGroup audioMxrGroup;
+		public List<Sound> musicList;
+		public bool randomize = false;
 
-		// Ensure only one manager exists
-		if (musicManager == null)
+		private int currentSongIndex = 0;
+		private List<Sound> haventPlayedList;
+
+		void Awake()
 		{
-			musicManager = this;
-			DontDestroyOnLoad(gameObject);
-		}
-		else Destroy(gameObject);
+			haventPlayedList = new List<Sound>();
 
-		foreach (Sound sound in musicList)
-		{
-			sound.source = gameObject.AddComponent<AudioSource>();
-			sound.source.clip = sound.clip;
-			sound.source.loop = sound.loop;
-			sound.source.outputAudioMixerGroup = audioMxrGroup;
+			// Ensure only one manager exists
+			if (musicManager == null)
+			{
+				musicManager = this;
+				DontDestroyOnLoad(gameObject);
+			}
+			else Destroy(gameObject);
 
-			haventPlayedList.Add(sound);
-		}
-	}
+			foreach (Sound sound in musicList)
+			{
+				sound.source = gameObject.AddComponent<AudioSource>();
+				sound.source.clip = sound.clip;
+				sound.source.loop = sound.loop;
+				sound.source.outputAudioMixerGroup = audioMxrGroup;
 
-	public static IEnumerator FadeOut(Sound sound, float FadeTime)
-	{
-		float startVolume = sound.volume;
-
-		while (sound.volume > 0)
-		{
-			sound.volume -= startVolume * Time.deltaTime / FadeTime;
-
-			yield return null;
+				haventPlayedList.Add(sound);
+			}
 		}
 
-		sound.source.Stop();
-		sound.volume = startVolume;
-	}
+		public Sound GetCurrentSong()
+		{
+			if (musicList.Count == 0) return null;
+			else return musicList[currentSongIndex];
+		}
 
-	public bool PlayNextSongInOrder()
-	{
-		if (currentSongIndex + 1 > musicList.Count) return false;
+		public static IEnumerator FadeOut(Sound sound, float FadeTime)
+		{
+			float startVolume = sound.volume;
 
-		if (currentSongIndex == 0)
-        {
-			musicList[currentSongIndex].source.Play();
+			while (sound.volume > 0)
+			{
+				sound.volume -= startVolume * Time.deltaTime / FadeTime;
+
+				yield return null;
+			}
+
+			sound.source.Stop();
+			sound.volume = startVolume;
+		}
+
+		public bool PlayNextSongInOrder()
+		{
+			if (currentSongIndex + 1 > musicList.Count) return false;
+
+			if (currentSongIndex == 0)
+			{
+				musicList[currentSongIndex].source.Play();
+				StartCoroutine(VerifyPlaying());
+				return true;
+			}
+
+			FadeOut(musicList[currentSongIndex], 1);
+			musicList[++currentSongIndex].source.Play();
+			gameObject.GetComponent<MusicAnalyzer>().UpdateSongPlaying();
+			Debug.Log("played song " + musicList[currentSongIndex].source.name);
+
 			StartCoroutine(VerifyPlaying());
 			return true;
 		}
 
-		FadeOut(musicList[currentSongIndex], 1);
-		musicList[++currentSongIndex].source.Play();
-		Debug.Log("played song " + musicList[currentSongIndex].source.name);
-
-		StartCoroutine(VerifyPlaying());
-		return true;
-	}
-
-	public bool PlayNextSongRandom()
-    {
-		if (haventPlayedList.Count == 0) return false;
-
-		var random = new System.Random();
-		int index = random.Next(haventPlayedList.Count);
-		haventPlayedList[index].source.Play();
-		Debug.Log("played song " + haventPlayedList[index].source.name);
-		haventPlayedList.RemoveAt(index);
-		return true;
-    }
-
-	IEnumerator VerifyPlaying()
-	{
-		while (true)
+		public bool PlayNextSongRandom()
 		{
-			yield return new WaitForSeconds(1f);
-			if (!musicList[currentSongIndex].source.isPlaying)
-            {
-				if (randomize)
-                {
-					if (PlayNextSongRandom()) Debug.Log("switched to next random song");
-					else Debug.Log("no songs left to play");
-				}
-				else
-                {
-					if (PlayNextSongInOrder()) Debug.Log("switched to next song");
-					else Debug.Log("no songs left to play for this level");
+			if (haventPlayedList.Count == 0) return false;
+
+			var random = new System.Random();
+			int index = random.Next(haventPlayedList.Count);
+			haventPlayedList[index].source.Play();
+			gameObject.GetComponent<MusicAnalyzer>().UpdateSongPlaying();
+			Debug.Log("played song " + haventPlayedList[index].source.name);
+			haventPlayedList.RemoveAt(index);
+
+			gameObject.GetComponent<MusicAnalyzer>().UpdateSongPlaying();
+			return true;
+		}
+
+		IEnumerator VerifyPlaying()
+		{
+			while (true)
+			{
+				yield return new WaitForSeconds(1f);
+				if (!musicList[currentSongIndex].source.isPlaying)
+				{
+					if (randomize)
+					{
+						if (PlayNextSongRandom()) Debug.Log("switched to next random song");
+						else Debug.Log("no songs left to play");
+					}
+					else
+					{
+						if (PlayNextSongInOrder()) Debug.Log("switched to next song");
+						else Debug.Log("no songs left to play for this level");
+					}
 				}
 			}
 		}
