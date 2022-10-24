290d4d59cd19b07a4636d3ddcbeaea9aac3ab783
Daniel Currey <spiceycurrey@gmail.com>
2022-10-23 22:25:50 -0700

    Added syncing with musicAnalyzer

--- Assets/Scripts/Surfaces/BasicSurface.cs
+++ Assets/Scripts/Surfaces/BasicSurface.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 namespace Detection
 {
     public class BasicSurface : MonoBehaviour, IScannable
-    {
+    { 
+
         [SerializeField] ParticleSystem _particleSystem;
+        [SerializeField] MusicAnalyzer musicAnalyzer;
+
         void IScannable.EmitParticle(Vector3 position, ParticleSystem overrideParticleSystem)
         {
             var emitArgs = new ParticleSystem.EmitParams();
             emitArgs.position = position;
             emitArgs.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            emitArgs.startSize = musicAnalyzer.currentLoudness;
 
             if (overrideParticleSystem == null)
             {
