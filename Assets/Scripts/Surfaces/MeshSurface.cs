8a70033139d697e290bb100eac2afcd208a117dc
Cameron <cameron.batchelor22@gmail.com>
2022-11-06 17:02:17 -0800

    Fixed override particle system in MeshSurface

--- Assets/Scripts/Surfaces/MeshSurface.cs
+++ Assets/Scripts/Surfaces/MeshSurface.cs
@@ -15,21 +15,21 @@ namespace Detection
                 emitArgs.position = hit.point;
                 emitArgs.velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
-                // Assumes there is a MeshRenderer AND a MeshCollider
-                Renderer renderer = hit.collider.GetComponent<MeshRenderer>();
-                Texture2D texture2D = renderer.material.mainTexture as Texture2D;
-                Vector2 pCoord = hit.textureCoord;
-                pCoord.x *= texture2D.width;
-                pCoord.y *= texture2D.height;
-
-                Vector2 tiling = renderer.material.mainTextureScale;
-                int x = Mathf.FloorToInt(pCoord.x * tiling.x);
-                int y = Mathf.FloorToInt(pCoord.y * tiling.y);
-                Color color = texture2D.GetPixel(x, y);
-                emitArgs.startColor = color;
-
                 if (overrideParticleSystem == null)
                 {
+                    // Assumes there is a MeshRenderer AND a MeshCollider
+                    Renderer renderer = hit.collider.GetComponent<MeshRenderer>();
+                    Texture2D texture2D = renderer.material.mainTexture as Texture2D;
+                    Vector2 pCoord = hit.textureCoord;
+                    pCoord.x *= texture2D.width;
+                    pCoord.y *= texture2D.height;
+
+                    Vector2 tiling = renderer.material.mainTextureScale;
+                    int x = Mathf.FloorToInt(pCoord.x * tiling.x);
+                    int y = Mathf.FloorToInt(pCoord.y * tiling.y);
+                    Color color = texture2D.GetPixel(x, y);
+                    emitArgs.startColor = color;
+
                     if (_particleSystem == null) return;
                     _particleSystem.Emit(emitArgs, 1);
                 }
