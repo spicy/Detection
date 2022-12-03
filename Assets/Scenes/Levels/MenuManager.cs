a3b486ef43686a5d117d4a98e7717d7dbed73547
jericc <jeric.baquiran@yahoo.com>
2022-12-03 01:58:31 -0800

    Main Menu Properly Switches When Settings Button Is Pressed

---
+++ Assets/Scenes/Levels/MenuManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Canvas;
+    void Start()
+    {
+        Canvas.SetActive(false);
+    }
+
+  
+
+    // Update is called once per frame
+}
